393848bdbe3b74cf763490cec3fecf535c2ee49b
consultorsandro <consultorsandro@hotmail.com>
2022-11-26 19:48:45 -0300

    "Como inserir registros na tabela"

---
+++ 02_Insere_reg_tabela.sql
@@ -0,0 +1,9 @@
+Inserindo registros na tabela
+
+insert into TAB_TESTE (ID, NOME, date)
+values (1, 'Sandro Reis', '26.11.2022')
+
+INSERT INTO TAB_TESTE
+values (2, 'NOME', '27.01.2020');
+
+INSERT INTO TAB_TESTE(ID) VALUES(3);
